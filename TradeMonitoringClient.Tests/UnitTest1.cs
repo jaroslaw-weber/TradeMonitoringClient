c307feb55a8ed243bdd5adc92d8d081f74deb3b1
Jaroslaw Weber <jaroslawweber@gmail.com>
2021-11-28 17:15:21 +0900

    scaffold tests project

---
+++ TradeMonitoringClient.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace TradeMonitoringClient.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
